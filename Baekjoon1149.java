4ded14a4b8a38cd4c547e0d31693e31bf3a8808a
HideOnCodec <64856490+HideOnCodec@users.noreply.github.com>
2023-02-11 22:27:48 +0900

    Update Baekjoon1149.java

--- Baekjoon1149.java
+++ Baekjoon1149.java
@@ -4,6 +4,46 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Main {
+    public static void main(String[] args) throws IOException {
+         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+         int n = Integer.parseInt(br.readLine());
+         int dp[][]=new int[n][3];
+         
+         for(int i=0; i<n; i++){
+             StringTokenizer st = new StringTokenizer(br.readLine()," ");
+             int R=Integer.parseInt(st.nextToken());
+             int G=Integer.parseInt(st.nextToken());
+             int B=Integer.parseInt(st.nextToken());
+             
+             if(i==0) {
+            	 dp[0][0]=R;
+            	 dp[0][1]=G;
+            	 dp[0][2]=B;
+             }
+             else {
+            	 dp[i][0]=Math.min(dp[i-1][1],dp[i-1][2])+R;//빨간색일 경우
+            	 dp[i][1]=Math.min(dp[i-1][0],dp[i-1][2])+G;//초록색일 경우
+            	 dp[i][2]=Math.min(dp[i-1][0],dp[i-1][1])+B;//파란색일 경우
+             }
+         }
+         
+         int min=dp[n-1][0];
+         for(int i=1; i<3; i++) {
+        	 if(min>dp[n-1][i])
+        		 min=dp[n-1][i];
+         }
+         
+         System.out.println(min);
+    }
+}
+
+/* 처음 틀렸을 때 코드
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
     public static int min(int past,int arr[]){
         int min = 1000;
         int index=-1;
@@ -41,3 +81,4 @@ public class Main {
          System.out.println(sum);
     }
 }
+*/
